d828f75cb69bb46ee6e67a1cfbbe599ff578b077
Axel Hahn <ax2002@gmx.net>
2024-10-10 00:29:23 +0200

    add generatr example

---
+++ scripts/generate_bashdoc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd "$( dirname "$0" )" || exit
+
+BASHDOC=./../bashdoc2md.sh
+
+SOURCES=./../bashdoc2md.sh
+TARGETBASE=./../docs/99_Functions/
+REPO=https://github.com/axelhahn/bashdoc/blob/main/
+
+
+echo
+echo "GENERATE MARKDOWN DOCS"
+echo
+
+for SOURCE in $SOURCES
+do
+    TARGET="$TARGETBASE/$(basename "$SOURCE").md"
+    if [ "$TARGET" -nt "$SOURCE" ]; then
+        echo "SKIP: $TARGET is up to date."
+    else
+        rm -f "$TARGET"
+        echo "Generating $TARGET ... "
+        $BASHDOC -l 2 -r "$REPO" "$SOURCE" > "$TARGET"
+    fi
+    ls -l "$SOURCE" "$TARGET"
+    echo
+done
+echo "Done."
